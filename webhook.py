17f75cf520225e50e4c520dcac8a518c9fd79f80
younmahon <47487209+younmahon@users.noreply.github.com>
2019-02-20 09:45:14 +0100

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -27,7 +27,7 @@ def webhook():
 
 
 def makeResponse(req):
-    result = req.get("result")
+    result = req.get("queryResult")
     parameters = result.get("parameters")
     city = parameters.get("geo-city")
     date = parameters.get("date")
