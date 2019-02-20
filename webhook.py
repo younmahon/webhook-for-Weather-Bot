2803e81366fbaaa0a8eecdc48c48ec0bd1a84c5f
younmahon <47487209+younmahon@users.noreply.github.com>
2019-02-20 22:13:50 +0100

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,5 +1,3 @@
-
-
 import json
 import os
 import requests
@@ -10,22 +8,17 @@ from flask import request
 from flask import make_response
 
 # Flas app should start in global layout
-
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
-
 def webhook():
    req = request.get_json(silent=True, force=True)
    print(json.dumps(req, indent=4))
-
    res = makeResponse(req)
    res = json.dumps(res, indent=4)
    r = make_response(res)
    r.headers['Content-Type'] = 'application/json'
    return r
-
-
 def makeResponse(req):
     result = req.get("queryResult")
     parameters = result.get("parameters")
@@ -38,14 +31,12 @@ def makeResponse(req):
          if date in weather[i]['dt_txt']:
             condition= weather[i]['weather'][0]['description']
             break
-
     speech = "The forcast for"+city+ "for "+date+" is "+condition
     return {
     "speech": speech,
     "displayText": speech,
     "source": "apiai-weather-webhook"
     }
-
 if __name__ == '__main__':
      port = int(os.getenv('PORT', 5000))
      print("Starting app on port %d" % port)
