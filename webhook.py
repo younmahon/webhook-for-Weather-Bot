eda7fa01951b36eb4eee3c64db542edf282e341e
younmahon <47487209+younmahon@users.noreply.github.com>
2019-02-20 18:43:53 +0100

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -37,9 +37,9 @@ def makeResponse(req):
     json_object = r.json()
     weather=json_object['list']
     for i in range(0,30):
-          if date in weather[i]['dt_txt']:
-              condition= weather[i]['weather'][0]['description']
-			  break
+         if date in weather[i]['dt_txt']:
+             condition= weather[i]['weather'][0]['description']
+	         break
 
 
     speech = "The forcast for"+city+ "for "+date+" is "+condition
