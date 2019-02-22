b5e7464b20ff2f92a497997a5556942ed6507ed8
younmahon <47487209+younmahon@users.noreply.github.com>
2019-02-22 17:39:45 +0100

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -26,17 +26,15 @@ def makeResponse(req):
     date = parameters.get("date")
     r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=b6907d289e10d714a6e88b30761fae22')
     json_object = r.json()
-    #weather=json_object['list']
-    #for i in range(0,30):
-    #     if date in weather[i]['dt_txt']:
-    #        condition= weather[i]['weather'][0]['description']
-    #        break
-    #speech = "The forcast for"+city+ "for "+date+" is "+condition
-    speech = "waaa hiaa waaa hiaaa"
+    weather=json_object['list']
+    for i in range(0,30):
+        print(i)
+         if date in weather[i]['dt_txt']:
+            condition= weather[i]['weather'][0]['description']
+            break
+    speech = "The forcast for"+city+ "for "+date+" is "+condition
     return {
     "speech": speech,
-
-
     "displayText": speech,
     "source": "apiai-weather-webhook"
     }
