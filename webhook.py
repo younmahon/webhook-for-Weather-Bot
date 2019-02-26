64820bc5a9cbefd5a6caf87a694fa8be308a91a7
younmahon <47487209+younmahon@users.noreply.github.com>
2019-02-26 12:56:40 +0100

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,126 +1,56 @@
-#!/usr/bin/env python
-
-import urllib
 import json
 import os
-import sys
+import requests
 
 from flask import Flask
 from flask import request
 from flask import make_response
+import plivo
 
 # Flask app should start in global layout
 app = Flask(__name__)
 
-
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-
-    print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
-    print("Request processed")
+    res = makeResponse(req)
 
     res = json.dumps(res, indent=4)
-    print(res)
-
+    # print(res)
     r = make_response(res)
-    print(r)
-
     r.headers['Content-Type'] = 'application/json'
     return r
 
-
-def processRequest(req):
-    if req.get("result").get("action") != "createCheckBook":
-        return {}
-    baseurl = "https://ldcif6u.wdf.sap.corp:44304/sap/opu/odata/sap/ZIVRC_SRV/WorkItems("
-    print(baseurl)
-
-    yql_query = makeYqlQuery(req)
-    print("yql_query1:")
-    print(yql_query)
-
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urllib.urlencode({yql_query}) + "?$format=json"
-    print(yql_query)
-
-    result = urllib.urlopen(baseurl).read()
-    print(result)
-    data = json.loads(result)
-    print("data1:")
-    print(data)
-
-    res = makeWebhookResult(data)
-    return res
-
-
-def makeYqlQuery(req):
-    result = req.get("result")
-    print("result1:")
-    print(result)
+def makeResponse(req):
+    result = req.get("queryResult")
     parameters = result.get("parameters")
-    print("parameters1:")
-    print(parameters)
-    city = parameters.get("workitemtype")
-    print("City1:")
-    print(city)
-    if city is None:
-        return None
-
-        return "guid'" + "0005EEE4-48CC-1ED5-B0C9-FA163EA701AC" + "')"
-   #return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-
-
-def makeWebhookResult(data):
-    print("MakeWebhook method")
-    query = data.get('d')
-    print("Query1:")
-    print(query)
-    if query is None:
-        return {}
-
-    result = query.get('WORKITEM_ID')
-    if result is None:
-        return {}
-
-    channel = query.get('DESCRIPTION')
-    if channel is None:
-        return {}
-
-   # item = channel.get('item')
-   # location = channel.get('location')
-   # units = channel.get('units')
-   # if (location is None) or (item is None) or (units is None):
-   #    return {}
-
-   # condition = item.get('condition')
-   # if condition is None:
-   #     return {}
-
-    # print(json.dumps(item, indent=4))
-
-   # speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-   #         ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-	speech = " The Work Item No. " + result + " has been created for " + channel
-    print("Response1:")
-    print(speech)
-
+    city = parameters.get("geo-city")
+    date = parameters.get("date")
+    r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=98fe55e4c365cbd0b333744cf7db0f78')
+    condition1= "nothing else matters"
+    json_object = r.json()
+    weather=json_object['list']
+    for i in range(0,30):
+        if date in weather[i]['dt_txt']:
+            condition1= weather[i]['weather'][0]['description']
+            break
+    condition = condition1
+    client = plivo.RestClient()
+    response = client.messages.create(
+    src='00491728080080',
+    dst='00491728080144',
+    text='Test Message', )
+    print(response)
+    speech = "The forecast for"+city+"for "+date+" is "+condition
     return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
+    "speech": speech,
+    "displayText": speech,
+    "source": "apiai-weather-webhook"
     }
-
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-
-    print "Starting app on port %d" % port
-
+    print("Starting app on port %d" % port)
     app.run(debug=False, port=port, host='0.0.0.0')
